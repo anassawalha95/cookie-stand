f0ec958749a58a1d71d704605d44d24aee1d1d1a
anasswalha95 <anas.swalha@yahoo.com>
2021-01-13 18:36:03 +0200

    finishing class09-forms

--- js/app.js
+++ js/app.js
@@ -3,6 +3,25 @@
 var seattle, tokyo, dubai, paris, lima, d = document, mainContainer = d.getElementById("sales-items");
 var laborHours = ["6:00am", "7:00am", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm", "6:00pm", "7:00pm"]
 
+var locations = [];
+var table = d.createElement("table")
+var thead = d.createElement("thead")
+var tbody = d.createElement("tbody")
+var tfoot = d.createElement("tfoot")
+
+function isEmpty(check) {
+
+    if (check != "") {
+
+        return true;
+
+    }
+
+
+    return false;
+
+}
+
 Shop.prototype.calculateAvarageSalesPerHour = function () {
 
     var randomNumberGenerated, laborHoursArrayLength = laborHours.length, HourlySales = [];
@@ -32,12 +51,12 @@ Shop.prototype.calculateTotalSalesPerHour = function () {
 
 
 
-
 Shop.prototype.tableDataFiller = function (tbody) {
 
-    var tr = d.createElement("tr")
+    var tr = d.createElement("tr");
 
-    for (var index = 0; index <= laborHours.length + 2; index++) {
+
+    for (var index = 0; index < laborHours.length + 2; index++) {
 
         var td = d.createElement("td")
 
@@ -57,18 +76,62 @@ Shop.prototype.tableDataFiller = function (tbody) {
         tr.appendChild(td)
 
     }
+
     tbody.appendChild(tr)
 };
 
 
+
+function tableTfootFiller(tfoot, locations) {
+
+    var tr = d.createElement("tr");
+    var total = 0;
+    var grandTotal = 0
+    for (var index = 0; index < 16; index++) {
+
+        var td = d.createElement("td")
+        if (index == 0) {
+            td.textContent = "Totals"
+
+        } else if (index == 15) {
+            td.textContent = grandTotal
+        }
+
+        else {
+
+            for (var index2 = 0; index2 < locations.length; index2++) {
+
+                total += locations[index2].salesPerHour[index - 1]
+                //grandTotal += total;
+
+            }
+            grandTotal += total;
+            td.textContent = total
+            total = 0
+        }
+
+
+
+        tr.appendChild(td)
+
+    }
+
+    tfoot.appendChild(tr)
+
+}
+
+
 function Shop(location, maxCustomerNumber, minCustomerNumber, averageCookieSales) {
     this.location = location;
-
     this.maxCustomerNumber = maxCustomerNumber;
     this.minCustomerNumber = minCustomerNumber;
     this.averageCookieSales = averageCookieSales;
     this.salesPerHour = this.calculateAvarageSalesPerHour();
+
     this.totalSalesPerDay = this.calculateTotalSalesPerHour();
+
+    locations.push(this)
+
 }
 
 
@@ -82,11 +145,80 @@ paris = new Shop("Paris", 38, 20, 2.3);
 
 lima = new Shop("Lima", 16, 2, 4.6);
 
-var locations = [seattle, tokyo, dubai, paris, lima]
+
+console.log(locations.length)
+
 var table = d.createElement("table")
 var thead = d.createElement("thead")
 var tbody = d.createElement("tbody")
 var tfoot = d.createElement("tfoot")
+
+
+var addNewItem = d.getElementById("submit");
+
+addNewItem.addEventListener("click", addNewStore)
+
+function addNewStore(e) {
+    e.preventDefault();
+
+    var newLocation = d.getElementById("location")
+    var newMinCustomerNumber = d.getElementById("minCustomerNumber")
+    var newMaxCustomerNumber = d.getElementById("maxCustomerNumber")
+    var newAvgSalesPerHour = d.getElementById("AvgSalesPerHour")
+
+
+    if (isEmpty(newLocation.value) && isEmpty(newMaxCustomerNumber.value) && isEmpty(newMinCustomerNumber.value) && isEmpty(newAvgSalesPerHour.value)) {
+        var newStore = new Shop(newLocation.value, parseInt(newMaxCustomerNumber.value), parseInt(newMinCustomerNumber.value), parseFloat(newAvgSalesPerHour.value));
+
+        console.log(locations)
+
+        mainContainer.innerHTML = ""
+
+
+
+        table = d.createElement("table")
+        thead = d.createElement("thead")
+        tbody = d.createElement("tbody")
+        tfoot = d.createElement("tfoot")
+
+        tableTheadFiller(thead)
+        for (var index = 0; index < locations.length; index++) {
+            locations[index].tableDataFiller(tbody)
+        }
+
+        tableTfootFiller(tfoot, locations)
+
+        table.appendChild(thead)
+        table.appendChild(tbody)
+        table.appendChild(tfoot)
+
+        mainContainer.appendChild(table)
+
+
+
+
+    } else {
+        if (isEmpty(newLocation)) {
+            newLocation.style.borderColor = "red"
+        }
+
+        if (isEmpty(newMaxCustomerNumber)) {
+            newMaxCustomerNumber.style.borderColor = "red"
+        }
+
+        if (isEmpty(newMinCustomerNumber)) {
+            newMinCustomerNumber.style.borderColor = "red"
+        }
+
+        if (isEmpty(newAvgSalesPerHour)) {
+            newAvgSalesPerHour.style.borderColor = "red"
+        }
+    }
+}
+
+
+
+
 
 
 function tableTheadFiller(thead) {
@@ -119,44 +251,6 @@ function tableTheadFiller(thead) {
 }
 
 
-
-function tableTfootFiller(tfoot, locations) {
-
-    var tr = d.createElement("tr");
-    var total = 0;
-    var grandTotal = 0
-    for (var index = 0; index < 16; index++) {
-
-        var td = d.createElement("td")
-        if (index == 0) {
-            td.textContent = "Totals"
-
-        } else if (index == 15) {
-            td.textContent = grandTotal
-        }
-
-        else {
-
-            for (var index2 = 0; index2 < locations.length; index2++) {
-                console.log(locations)
-                total += locations[index2].salesPerHour[index - 1]
-                //grandTotal += total;
-
-            }
-            grandTotal += total;
-            td.textContent = total
-            total = 0
-        }
-
-
-
-        tr.appendChild(td)
-
-    }
-
-    tfoot.appendChild(tr)
-
-}
 
 tableTheadFiller(thead)
 
